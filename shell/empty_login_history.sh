97ce555a478f046cd3b3ec283ee6a940ae1d7afd
mack-a <57424792+mack-a@users.noreply.github.com>
2020-11-21 16:40:27 +0800

    feat(脚本): update

--- shell/empty_login_history.sh
+++ shell/empty_login_history.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 # 清空访问日志
-# wget -P /tmp https://raw.githubusercontent.com/mack-a/v2ray-agent/master/shell/empty_login_history.sh && chmod 700 /tmp/empty_login_history.sh && bash /tmp/empty_login_history.sh
+# wget -P /tmp -N --no-check-certificate "https://raw.githubusercontent.com/mack-a/v2ray-agent/master/shell/empty_login_history.sh" && chmod 700 /tmp/empty_login_history.sh && /tmp/empty_login_history.sh
+# wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/mack-a/v2ray-agent/master/install.sh" && chmod 700 /root/install.sh && /root/install.sh
+echo "清空中"
 echo > /var/log/wtmp
 echo > /var/log/btmp
+echo > /var/log/lastlog
 echo > ~/.bash_history
+echo "清空完毕"
+echo "删除脚本"
 rm -rf /tmp/empty_login_history.sh
 history -c
+echo "done"
