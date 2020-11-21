b81ae506b46a46cbc6e261c5c8b39fa035867ca0
mack-a <57424792+mack-a@users.noreply.github.com>
2020-11-21 16:26:15 +0800

    feat(脚本): update

--- shell/empty_login_history.sh
+++ shell/empty_login_history.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # 清空访问日志
+# wget -P /tmp https://raw.githubusercontent.com/mack-a/v2ray-agent/master/shell/empty_login_history.sh && chmod 700 /tmp/empty_login_history.sh && bash /tmp/empty_login_history.sh
 echo > /var/log/wtmp
 echo > /var/log/btmp
 echo > ~/.bash_history
+rm -rf /tmp/empty_login_history.sh
 history -c
