db8c5980dc2b36301e87667be0d2a26290c964d3
mack-a <57424792+mack-a@users.noreply.github.com>
2021-05-27 22:52:51 +0800

    feat(脚本): 关闭nginx不需要对外访问的端口

--- install.sh
+++ install.sh
@@ -3796,7 +3796,7 @@ menu() {
 	cd "$HOME" || exit
 	echoContent red "\n=============================================================="
 	echoContent green "作者：mack-a"
-	echoContent green "当前版本：v2.4.28"
+	echoContent green "当前版本：v2.4.29"
 	echoContent green "Github：https://github.com/mack-a/v2ray-agent"
 	echoContent green "描述：八合一共存脚本\c"
 	showInstallStatus
