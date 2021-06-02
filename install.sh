57ae1a432f6cdc8388eac19a92a543125547573f
mack-a <57424792+mack-a@users.noreply.github.com>
2021-06-02 18:01:25 +0800

    fix(脚本): 修改VLESS错误格式

--- install.sh
+++ install.sh
@@ -2311,7 +2311,7 @@ showAccounts() {
 			jq .inbounds[0].settings.clients ${configPath}02_VLESS_TCP_inbounds.json | jq -c '.[]' | while read -r user; do
 				echoContent skyBlue "\n ---> 帐号：$(echo "${user}" | jq .email | awk -F "[\"]" '{print $2}')_$(echo "${user}" | jq .id | awk -F "[\"]" '{print $2}')"
 				echo
-				defaultBase64Code vlesstcp $(echo "${user}" | jq .email)_$(echo "${user}" | jq .id) "${currentHost}:${currentPort}" ${currentHost}
+				defaultBase64Code vlesstcp $(echo "${user}" | jq .email) $(echo "${user}" | jq .id) "${currentHost}:${currentPort}" ${currentHost}
 			done
 		fi
 
@@ -3751,7 +3751,7 @@ menu() {
 	cd "$HOME" || exit
 	echoContent red "\n=============================================================="
 	echoContent green "作者：mack-a"
-	echoContent green "当前版本：v2.4.32"
+	echoContent green "当前版本：v2.4.33"
 	echoContent green "Github：https://github.com/mack-a/v2ray-agent"
 	echoContent green "描述：八合一共存脚本\c"
 	showInstallStatus
