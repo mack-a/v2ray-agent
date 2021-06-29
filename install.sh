5c43b55eaf676f56f3759bf8ca8a5b087241cb1a
mack-a <57424792+mack-a@users.noreply.github.com>
2021-06-29 11:07:33 +0800

    fix(脚本): 移除keepalive_time增加兼容性

--- install.sh
+++ install.sh
@@ -4074,7 +4074,7 @@ menu() {
 	cd "$HOME" || exit
 	echoContent red "\n=============================================================="
 	echoContent green "作者：mack-a"
-	echoContent green "当前版本：v2.5.11"
+	echoContent green "当前版本：v2.5.12"
 	echoContent green "Github：https://github.com/mack-a/v2ray-agent"
 	echoContent green "描述：八合一共存脚本\c"
 	showInstallStatus
