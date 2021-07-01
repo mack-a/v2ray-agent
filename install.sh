88f9a77c00a8fac6b1ea9970f1f346e0905ff7ac
mack-a <57424792+mack-a@users.noreply.github.com>
2021-07-01 21:09:44 +0800

    feat(version): update

--- install.sh
+++ install.sh
@@ -4200,7 +4200,7 @@ menu() {
 	cd "$HOME" || exit
 	echoContent red "\n=============================================================="
 	echoContent green "作者：mack-a"
-	echoContent green "当前版本：v2.5.14_dev_trojan"
+	echoContent green "当前版本：v2.5.14_dev_trojan_1"
 	echoContent green "Github：https://github.com/mack-a/v2ray-agent"
 	echoContent green "描述：八合一共存脚本\c"
 	showInstallStatus
