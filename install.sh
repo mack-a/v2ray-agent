e27a0890552558e567b363e607e20c176b865035
mack-a <57424792+mack-a@users.noreply.github.com>
2022-04-21 11:55:06 +0800

    feat(版本号): upgrade

--- install.sh
+++ install.sh
@@ -4550,7 +4550,7 @@ menu() {
 	cd "$HOME" || exit
 	echoContent red "\n=============================================================="
 	echoContent green "作者:mack-a"
-	echoContent green "当前版本:v2.5.59"
+	echoContent green "当前版本:v2.5.60"
 	echoContent green "Github:https://github.com/mack-a/v2ray-agent"
 	echoContent green "描述:八合一共存脚本\c"
 	showInstallStatus
