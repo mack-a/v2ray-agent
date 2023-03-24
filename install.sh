d105628e30ab5637fb80dc32e46163d11b5ed01c
mack-a <57424792+mack-a@users.noreply.github.com>
2023-03-24 16:17:25 +0800

    feat(脚本): 版本v2.7.19_reality_beta

--- install.sh
+++ install.sh
@@ -5930,7 +5930,7 @@ menu() {
     cd "$HOME" || exit
     echoContent red "\n=============================================================="
     echoContent green "作者：mack-a"
-    echoContent green "当前版本：v2.7.18_reality_beta"
+    echoContent green "当前版本：v2.7.19_reality_beta"
     echoContent green "Github：https://github.com/mack-a/v2ray-agent"
     echoContent green "描述：八合一共存脚本\c"
     showInstallStatus
