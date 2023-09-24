38476fb2186908006a8f7e412d8057f6379edc14
kawan1355 <134284170+kawan1355@users.noreply.github.com>
2023-09-24 14:50:46 +0330

    Update install.sh
    test

--- install.sh
+++ install.sh
@@ -7909,7 +7909,7 @@ menu() {
     echoContent green "年付10美金低价VPS AS4837：https://www.v2ray-agent.com/archives/racknerdtao-can-zheng-li-nian-fu-10mei-yuan"
     echoContent red "=============================================================="
     if [[ -n "${coreInstallType}" ]]; then
-        echoContent yellow "1.重新安装"
+        echoContent yellow "1.نصب"
     else
         echoContent yellow "1.安装"
     fi
