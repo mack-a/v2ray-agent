50deeea83db4651b49016702c7b3bbaab0553122
mack-a <57424792+mack-a@users.noreply.github.com>
2023-12-21 14:46:11 +0800

    feat(脚本): 增加sing-box核心

--- install.sh
+++ install.sh
@@ -8269,7 +8269,7 @@ menu() {
     cd "$HOME" || exit
     echoContent red "\n=============================================================="
     echoContent green "作者：mack-a"
-    echoContent green "当前版本：v3.1.12-beta"
+    echoContent green "当前版本：v3.1.13"
     echoContent green "Github：https://github.com/mack-a/v2ray-agent"
     echoContent green "描述：八合一共存脚本\c"
     showInstallStatus
