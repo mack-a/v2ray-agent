ae8270c3c51acd6b24aea4c58dd502da43b3a04a
mack-a <57424792+mack-a@users.noreply.github.com>
2024-03-05 23:35:29 +0800

    fix(脚本): 修复订阅节点域名读取问题

--- install.sh
+++ install.sh
@@ -770,7 +770,7 @@ readConfigHostPathUUID() {
     if [[ -f "/etc/v2ray-agent/cdn" ]] && grep -q "address" "/etc/v2ray-agent/cdn"; then
         currentCDNAddress=$(grep "address" "/etc/v2ray-agent/cdn" | awk -F "[:]" '{print $2}')
     else
-        currentCDNAddress=currentHost
+        currentCDNAddress="${currentHost}"
     fi
 }
 
@@ -8693,7 +8693,7 @@ menu() {
     cd "$HOME" || exit
     echoContent red "\n=============================================================="
     echoContent green "作者：mack-a"
-    echoContent green "当前版本：v3.2.13"
+    echoContent green "当前版本：v3.2.14"
     echoContent green "Github：https://github.com/mack-a/v2ray-agent"
     echoContent green "描述：八合一共存脚本\c"
     showInstallStatus
