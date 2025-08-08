55de7f6a7e29178c13994371896a6cc2910beb77
mack-a <57424792+mack-a@users.noreply.github.com>
2025-08-08 17:49:45 +0800

    feat(脚本): 完善singbox配置

--- install.sh
+++ install.sh
@@ -9931,7 +9931,7 @@ menu() {
     cd "$HOME" || exit
     echoContent red "\n=============================================================="
     echoContent green "作者：mack-a"
-    echoContent green "当前版本：v3.4.18"
+    echoContent green "当前版本：v3.4.19"
     echoContent green "Github：https://github.com/mack-a/v2ray-agent"
     echoContent green "描述：八合一共存脚本\c"
     showInstallStatus
