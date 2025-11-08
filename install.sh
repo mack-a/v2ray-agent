c5bad6804403a35b02b7358515eadbb43a3c5a48
mack-a <57424792+mack-a@users.noreply.github.com>
2025-11-08 21:54:41 +0800

    fix(脚本): 修复singbox anytls添加用户不成功问题、修复singbox证书过期无法生效问题

--- install.sh
+++ install.sh
@@ -2259,7 +2259,7 @@ renewalTLS() {
             if [[ "${coreInstallType}" == "1" ]]; then
                 handleXray stop
             elif [[ "${coreInstallType}" == "2" ]]; then
-                handleV2Ray stop
+                handleSingBox stop
             fi
 
             sudo "$HOME/.acme.sh/acme.sh" --cron --home "$HOME/.acme.sh"
@@ -6067,6 +6067,14 @@ addUser() {
             clients=$(jq -r "${userConfig} = ${clients}" ${configPath}11_VMess_HTTPUpgrade_inbounds.json)
             echo "${clients}" | jq . >${configPath}11_VMess_HTTPUpgrade_inbounds.json
         fi
+        # anytls
+        if echo "${currentInstallProtocolType}" | grep -q ",13,"; then
+            local clients=
+            clients=$(initSingBoxClients 13 "${uuid}" "${email}")
+
+            clients=$(jq -r "${userConfig} = ${clients}" ${configPath}13_anytls_inbounds.json)
+            echo "${clients}" | jq . >${configPath}13_anytls_inbounds.json
+        fi
     done
     reloadCore
     echoContent green " ---> 添加完成"
@@ -9496,7 +9504,7 @@ menu() {
     cd "$HOME" || exit
     echoContent red "\n=============================================================="
     echoContent green "作者：mack-a"
-    echoContent green "当前版本：v3.4.32"
+    echoContent green "当前版本：v3.4.33"
     echoContent green "Github：https://github.com/mack-a/v2ray-agent"
     echoContent green "描述：八合一共存脚本\c"
     showInstallStatus
